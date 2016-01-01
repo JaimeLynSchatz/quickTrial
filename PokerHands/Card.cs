ee666c2ba05f7a1f7d7d4a3f0233747de6cdf51a
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2016-01-01 06:33:14 -0800

    initial commit, includes Card class

---
+++ PokerHands/Card.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Card
+{
+	public Card()
+	{
+        private string rank;
+
+        //public string rank { get; set; }
+        //public string suit { get; set; }
+    }
+}
